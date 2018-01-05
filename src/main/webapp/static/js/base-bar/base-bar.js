257c71d3b9e60c86c73bd6aec09a5dc3d6907243
engikeer <engikeer@163.com>
2018-01-05 23:07:55 +0800

    update

--- src/main/webapp/static/js/base-bar/base-bar.js
+++ src/main/webapp/static/js/base-bar/base-bar.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
         title: {
             text: 'ECharts 入门示例'
         },
-        tooltip: {},
         legend: {
             data:['销量']
         },
@@ -14,6 +13,17 @@ $(document).ready(function() {
             data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
         },
         yAxis: {},
+        tooltip: {},
+        toolbox: {
+            feature: {
+                saveAsImage: {},
+                dataView: {},
+                dataZoom: {},
+                magicType: {
+                    type: ['line', 'bar', 'stack', 'tiled']
+                }
+            }
+        },
         series: [{
             name: '销量',
             type: 'bar',
